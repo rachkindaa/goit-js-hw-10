4e5aa28c51322aadd19b3e3df2471c4ff636f1a8
rachkindaa <rachkindaa@gmail.com>
2023-10-17 19:58:56 +0300

    a

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 import Notiflix from 'notiflix';
